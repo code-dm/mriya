7c1ac7c7aa2423b1683c329ea32febee2a719bfb
wudongming <wdmcode@aliyun.com>
2020-06-04 14:30:21 +0800

    update AggregateMessage, use MergeData to control message merge

--- mirya-flink/src/main/java/com/codingdm/mriya/aggregate/AggregateMessage.java
+++ mirya-flink/src/main/java/com/codingdm/mriya/aggregate/AggregateMessage.java
@@ -1,13 +1,14 @@
 package com.codingdm.mriya.aggregate;
 
-import com.codingdm.mriya.common.enums.EventType;
+import com.codingdm.mriya.enums.EventType;
+import com.codingdm.mriya.model.MergeData;
 import com.codingdm.mriya.model.Message;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.flink.api.common.functions.AggregateFunction;
 
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * @author wudongming1
@@ -16,56 +17,38 @@ import java.util.Map;
  **/
 @Slf4j
 public class AggregateMessage implements AggregateFunction<Message, Message, Message> {
-//    private Map<String, Map<String, String>> deleteData;
-//    private Map<String, Map<String, String>> inOrUpData;
-//    private StringBuilder sqlBuilder;
 
     @Override
     public Message createAccumulator() {
-//        deleteData = new HashMap<>();
-//        inOrUpData = new HashMap<>();
-//        sqlBuilder = new StringBuilder();
         return null;
     }
 
     /**
-     * @param message
+     * @param inMsg
      * @param accumulator
      * @return
      */
     @Override
-    public Message add(Message message, Message accumulator) {
-        if (accumulator == null ) {
-            accumulator = message;
-        } else {
-            if (message.typeIsDdl()) {
-                accumulator.setSql(accumulator.getSql() + ";" + message.getSql());
+    public Message add(Message inMsg, Message accumulator) {
+        if (accumulator == null) {
+            accumulator = inMsg;
+        }
+        boolean isMerge = inMsg.getData() != null
+                && !inMsg.typeIsDdl();
+        if (inMsg.typeIsDdl()) {
+            accumulator.setSql(accumulator.getSql() + ";" + inMsg.getSql());
+        }else if(isMerge){
+            Set<MergeData> mergeData = accumulator.getMergeData();
+            if(mergeData == null){
+                mergeData = new HashSet<>();
             }
-            if(message.getType() == EventType.DELETE && message.getData() != null){
-                for (Map<String, String> delete : message.getData()) {
-                    String pkValuesIds = message.getPkValuesIds(delete);
-                    Map<String, Map<String, String>> deleteData = accumulator.getDeleteData();
-                    if(deleteData == null){
-                        deleteData = new HashMap<>();
-                    }
-                    deleteData.put(pkValuesIds, delete);
-                    accumulator.setDeleteData(deleteData);
-                }
+            for (Map<String, String> d : inMsg.getData()) {
+                String pkValuesIds = inMsg.getPkValuesIds(d);
+                MergeData mg = new MergeData(pkValuesIds, inMsg.getType(), d);
+                mergeData.remove(mg);
+                mergeData.add(mg);
             }
-            boolean b = (message.getType() == EventType.INSERT
-                    || message.getType() == EventType.UPDATE)
-                    && message.getData() != null;
-            if(b){
-                for (Map<String, String> inOrUp : message.getData()) {
-                    String pkValuesIds = message.getPkValuesIds(inOrUp);
-                    Map<String, Map<String, String>> inOrUpData = accumulator.getDeleteData();
-                    if(inOrUpData == null){
-                        inOrUpData = new HashMap<>();
-                    }
-                    inOrUpData.put(pkValuesIds, inOrUp);
-                    accumulator.setInOrUpData(inOrUpData);
-                }
-            }
+            accumulator.setMergeData(mergeData);
         }
         accumulator.setData(null);
         return accumulator;
@@ -73,9 +56,7 @@ public class AggregateMessage implements AggregateFunction<Message, Message, Mes
 
     @Override
     public Message getResult(Message message) {
-//        message.setDeleteData(deleteData);
-//        message.setInOrUpData(inOrUpData);
-//        message.setSql(sqlBuilder.toString());
+
         return message;
     }
 
